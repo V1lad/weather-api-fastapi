78d50ce02f9e50dfc655f881a5564a51355822b0
Петров Владислав Дмитриевич <petrovvd@student.bmstu.ru>
2025-09-10 20:08:13 +0300

    Added schema for response validation

--- app/api/pydantic_schemas.py
+++ app/api/pydantic_schemas.py
@@ -1,7 +1,17 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 class WeatherRequestBase(BaseModel):
     lat: Optional[float] = None
     lon: Optional[float] = None
 
+class WeatherResponse(BaseModel):
+    id: int
+    latitude: Optional[float]
+    longitude: Optional[float]
+    request_time: datetime
+    temperature: float
+    
+    class Config:
+        from_attributes = True
