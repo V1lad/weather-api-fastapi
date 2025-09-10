f9fad231a80ac764a33d992e8e9aa7e92508e1ab
Петров Владислав Дмитриевич <petrovvd@student.bmstu.ru>
2025-09-10 20:34:54 +0300

    Added router initialization

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from app.db.models import Base
 from app.db.session import engine
+from app.api import weather_api
 
 # Создание таблицы
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Weather API", version="1.0.0")
+
+app.include_router(weather_api.router, prefix="/api/v1", tags=["weather"])
 
 @app.get("/")
 async def root():
