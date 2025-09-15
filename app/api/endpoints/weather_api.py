db156ce81e9a3852dcaf91e139111c501c74c33a
Петров Владислав Дмитриевич <pvlad29092003@mail.ru>
2025-09-15 17:14:19 +0300

    Moved endpoints to new endpoints folder

--- app/api/endpoints/weather_api.py
+++ app/api/endpoints/weather_api.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from typing import Optional
 
 from app.db.session import get_db
 from app.db.repository import WeatherRequestRepository
-from app.external_api.openweather_api import OpenWeatherAPI
+from app.core.openweatherapi_handler import OpenWeatherAPI
 from app.core.weather_service import WeatherService
 from app.api.pydantic_schemas import WeatherRequestBase, WeatherResponse
 
