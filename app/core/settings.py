99b640bbfa27f87dc24c78c63b7192a70dd47ea8
Петров Владислав Дмитриевич <pvlad29092003@mail.ru>
2025-09-15 15:03:18 +0300

    Added postgres parameters to settings and env

--- app/core/settings.py
+++ app/core/settings.py
@@ -5,7 +5,10 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./weather.db"
     OPENWEATHER_API_KEY: str
     OPENWEATHER_BASE_URL: str = "https://api.openweathermap.org/data/2.5"
-    
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
     class Config:
         env_file = ".env"
 
