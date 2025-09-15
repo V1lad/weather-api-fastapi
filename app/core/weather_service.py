7d08c90c382b12ddabe3ac741032edf3c4ec423f
Петров Владислав Дмитриевич <pvlad29092003@mail.ru>
2025-09-15 17:28:41 +0300

    Added comments

--- app/core/weather_service.py
+++ app/core/weather_service.py
@@ -2,6 +2,7 @@ from app.external_api.openweather_api import OpenWeatherAPI
 from app.db.models import WeatherRequest
 from app.db.repository import WeatherRequestRepository
 
+# Получает информацию от внешнего API
 class WeatherService:
     def __init__(self, weather_api: OpenWeatherAPI, repository: WeatherRequestRepository):
         self.weather_api = weather_api
@@ -11,7 +12,7 @@ class WeatherService:
         # Получаем данные от модуля работы с внешним API
         weather_data = self.weather_api.get_weather_by_coords(lat, lon)
         
-        # Создаем запись в БД
+        # Создание записи в БД
         db_weather_request = WeatherRequest(
             latitude=lat,
             longitude=lon,
