a32d859016b29b4b819ce9306816070232a192a1
Петров Владислав Дмитриевич <pvlad29092003@mail.ru>
2025-09-15 17:53:07 +0300

    Added weather_service logic to weather_api

--- app/api/endpoints/weather_api.py
+++ app/api/endpoints/weather_api.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.db.repository import WeatherRequestRepository
 from app.core.openweatherapi_handler import OpenWeatherAPI
-from app.core.weather_service import WeatherService
 from app.api.pydantic_schemas import WeatherRequestBase, WeatherResponse
+from app.db.models import WeatherRequest
 
 router = APIRouter()
 
@@ -29,7 +29,8 @@ def get_weather_service(
 @router.post("/weather", response_model=WeatherResponse)
 async def get_weather(
     request: WeatherRequestBase,
-    weather_service: WeatherService = Depends(get_weather_service)
+    openweatherapi_handler: OpenWeatherAPI = Depends(get_weather_api_connector),
+    db: WeatherRequestRepository = Depends(get_weather_repository)
 ):
     if not (request.lat is not None and request.lon is not None and request.lon <= 180 and
         request.lon >= -180 and request.lat <= 90 and request.lat >= -90):
@@ -39,5 +40,18 @@ async def get_weather(
         )
         
     return weather_service.get_weather_by_coords(request.lat, request.lon)
+    
+    # Получаем данные от модуля работы с внешним API
+    weather_data = openweatherapi_handler.get_weather_by_coords(request.lat, request.lon)
+    
+    # Создание записи в БД
+    db_weather_request = WeatherRequest(
+        latitude=request.lat,
+        longitude=request.lon,
+        temperature=weather_data["main"]["temp"]
+    )
+    
+    return db.create(db_weather_request)
+
 
 
