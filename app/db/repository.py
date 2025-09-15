fd18268efe80c6c16ce676a8f1d5606fb8e85ca3
Петров Владислав Дмитриевич <pvlad29092003@mail.ru>
2025-09-15 18:28:16 +0300

    Changed create functionality
    Weather repository is now creating records from parameters

--- app/db/repository.py
+++ app/db/repository.py
@@ -6,7 +6,14 @@ class WeatherRequestRepository:
     def __init__(self, db: Session):
         self.db = db
     
-    def create(self, weather_request: WeatherRequest) -> WeatherRequest:
+    # Создаёт запись о запросе, вовращает полученный результат
+    def create_weather_request(self, lat:float, lon:float, temp:float) -> WeatherRequest:
+        weather_request = WeatherRequest(
+            latitude=lat,
+            longitude=lon,
+            temperature=temp
+        )
+            
         self.db.add(weather_request)
         self.db.commit()
         self.db.refresh(weather_request)
