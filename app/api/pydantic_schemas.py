93e0e257521334a7e1e82f33494e8a4a219059c5
Петров Владислав Дмитриевич <pvlad29092003@mail.ru>
2025-09-15 17:59:28 +0300

    Added comments

--- app/api/pydantic_schemas.py
+++ app/api/pydantic_schemas.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 
+# Для валидации запросов с целью получения температуры
 class WeatherRequestBase(BaseModel):
     lat: float
     lon: float
 
+# Для валидации ответа сервера с температурой
 class WeatherResponse(BaseModel):
     temperature: float
     
